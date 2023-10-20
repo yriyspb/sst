3d4a83af0827247e75b7136791a648c128906df2
Yury <yriyspb@yandex.ru>
2023-10-20 05:26:34 +0300

    Selenium grid launch

--- conftest.py
+++ conftest.py
@@ -4,8 +4,13 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 @pytest.fixture(scope="function")
 def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    print("\nstart grid for test..")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.set_capability("platformName", "Windows 10")
+    browser = webdriver.Remote(
+        command_executor='http://localhost:4444',
+        options=chrome_options
+    )
     yield browser
     print("\nquit browser..")
     browser.quit()
